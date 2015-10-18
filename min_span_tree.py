6f435b04451098ebe2a0ade4cd93f5b26d9cc34d
Gabriel Levine <gabriel@magnetic.com>
2015-10-18 14:19:00 -0400

    added heapq to outside list as well. found minial speed savings

--- min_span_tree.py
+++ min_span_tree.py
@@ -9,26 +9,25 @@ def Edge(points):
     return m.sqrt(m.pow(m.fabs(a[0] - b[0]), 2) + m.pow(m.fabs(a[1] - b[1]), 2))
 
 
-def Min_Span_Tree(points):
+def Min_Span_Tree(outside_tree):
     #find all edges & put into a priorityQ
     heap = []
-    for pair in itertools.combinations(points, 2):
+    for pair in itertools.combinations(outside_tree, 2):
         heapq.heappush(heap, (Edge(pair), pair))
 
     inside_tree = set()
     answer = 0
-
-    outside_tree = points
-
+    heapq.heapify(list(outside_tree))
     initial = outside_tree.pop()
 
     while outside_tree:
         inside_tree.update(initial)
 
         shortest = min([item for item in heap if initial in item[1]])
-        answer += shortest[0]
+        answer += float(shortest[0])
         x, y = shortest[1]
         inside_tree.update(x, y)
+        heap.remove(shortest)
 
         next_point = outside_tree.pop()
 
